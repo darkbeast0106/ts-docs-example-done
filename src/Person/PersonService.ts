1d3110e624b56bbfbdad3dd8a8d43dfc893f1073
Petrik Diak <diak@petrik.hu>
2023-11-27 15:37:40 +0100

    PersonService osztály dokumentálása

--- src/Person/PersonService.ts
+++ src/Person/PersonService.ts
@@ -1,9 +1,21 @@
-import Person from "./Person";
+import { Person } from "./Person";
 import { PersonDTO } from "./PersonDTO";
 
+/**
+ * Emberekre valósít meg CRUD-ot. (Create Read Update Delete)
+ * Az adatok forrása az api_url tagváltozóban található retool api.
+ */
 export class PersonService {
+  /**
+   * Az adatokat szolgáltató retool api backend urlje.
+   */
   api_url = "https://retoolapi.dev/78KAb0/people";
 
+  /**
+   * Létrehoz egy új embert az adatbázisban.
+   * @param person Az új ember adatai. Nem tartalmaz azonosítót.
+   * @returns Létrehozott ember adatai. A szerver által generált azonosítót is tartalmazza.
+   */
   async add(person: PersonDTO): Promise<Person> {
     const response = await fetch(this.api_url, {
       method: "POST",
@@ -19,6 +31,10 @@ export class PersonService {
     return response.json();
   }
 
+  /**
+   * Lekérdezi az embereket a szerverről
+   * @returns Embereket tartalmazó lista
+   */
   async getAll(): Promise<Person[]> {
     const response = await fetch(this.api_url, {
       headers: { Accept: "application/json" },
@@ -29,6 +45,12 @@ export class PersonService {
     return response.json();
   }
 
+  /**
+   * Módosít egy meghatározott embert az adatbázisban.
+   * @param id A módosítandó ember azonosítója
+   * @param person Az ember új adatai, azonosító nélkül.
+   * @returns A módosított adatok azonosítóval együtt.
+   */
   async update(id: number, person: PersonDTO): Promise<Person> {
     const response = await fetch(`${this.api_url}/${id}`, {
       method: "PATCH",
@@ -44,6 +66,11 @@ export class PersonService {
     return response.json();
   }
 
+  /**
+   * Töröl meghatározott embert az adatbázisból.
+   * @param id Törlendő ember azonosítója
+   * @returns Törlés sikeressége.
+   */
   async delete(id: number): Promise<boolean> {
     const response = await fetch(`${this.api_url}/${id}`, {
       method: "DELETE",
@@ -52,6 +79,6 @@ export class PersonService {
     if (!response.ok) {
       throw new Error("An error occured while adding deleting a person");
     }
-    return response.json();
+    return true;
   }
 }
