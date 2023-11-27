fb1f5ce3b3aaf565f2daf89c3eb8a9a436d4f11a
Petrik Diak <diak@petrik.hu>
2023-11-27 15:36:30 +0100

    Person interfész dokumentálása

--- src/Person/Person.ts
+++ src/Person/Person.ts
@@ -1,6 +1,21 @@
-export default interface Person {
+/**
+ * Ember tulajdonságait leíró interfész
+ */
+export interface Person {
+  /**
+   * Ember azonosítója
+   */
   id: number;
+  /**
+   * Ember teljes neve
+   */
   name: string;
+  /**
+   * Ember e-mail címe
+   */
   email: string;
+  /**
+   * Ember munkaköre
+   */
   job: string;
 }
